5361335fb597ea02dcd9620168881cb1f03988b9
yar <yar2022789@gmail.com>
2026-06-28 20:59:43 +0300

    add registration

--- core/users/router.py
+++ core/users/router.py
@@ -7,21 +7,3 @@ from sqlalchemy import select
 from models.models import User
 
 router = APIRouter(prefix="/users")
-
-@router.post("", status_code=status.HTTP_201_CREATED)
-async def create_user(request: CreateUser, db = Depends(get_session)):
-    user = User(
-        name = request.name,
-        email = request.email,
-        hashed_password = bcrypt.hashpw(request.password, bcrypt.gensalt()).decode("utf-8"),
-        role = request.role
-    )
-    try:
-        db.add(user)
-        await db.commit()
-    except IntegrityError:
-        await db.rollback()
-        raise HTTPException(status_code=409, detail="Database error")
-    
-    return "done"
-
