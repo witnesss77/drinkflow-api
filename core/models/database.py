21099f86fa41bf8c140bd35fd1ec34e43f4a0564
yar <yar2022789@gmail.com>
2026-07-31 17:46:08 +0300

    basic route tests

--- core/models/database.py
+++ core/models/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 database_url = cfg.db_str
 
-engine = create_async_engine(url=database_url, pool_pre_ping = True)
+engine = create_async_engine(url=database_url, pool_pre_ping = True) #poolclass = NullPool на тестах 
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 async def get_session():
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,8 +3,21 @@ from httpx import AsyncClient, ASGITransport
 from core.main import app
 
 
+# @pytest.mark.asyncio
+# async def test_get_drinks():
+#     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+#         response = await ac.get("/drinks")
+#         data = response.json()
+#         assert data != []
+
 @pytest.mark.asyncio
-async def test_get_drinks():
+async def test_set_drinks():
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        response = await ac.get("/drinks")
-        print(response)
+        response = await ac.post("/drinks", json={
+            "name": "test",
+            "desc": "none",
+            "alcoholic": False,
+            "price": 111,
+            "factory_id": 1
+        })
+        assert response.status_code == 201
